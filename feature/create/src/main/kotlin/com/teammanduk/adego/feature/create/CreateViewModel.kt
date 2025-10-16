03f150a3719a1b45078a0c17014982b8ce098896
bong-pal <etgt777@naver.com>
2025-10-16 14:01:11 +0900

    fix: Create 화면 navOptions 제거로 ViewModel 생명주기 개선
    - MainNavigator.navigateToCreate()에서 navOptions 제거
    - 매번 새로운 백스택 엔트리 생성으로 Create 재진입 시 새 ViewModel 생성
    - DisposableEffect와 resetState() 제거 (더 이상 불필요)

--- feature/create/src/main/kotlin/com/teammanduk/adego/feature/create/CreateViewModel.kt
+++ feature/create/src/main/kotlin/com/teammanduk/adego/feature/create/CreateViewModel.kt
@@ -46,10 +46,6 @@ class CreateViewModel @Inject constructor(
         }
     }
 
-    fun resetState() {
-        _uiState.update { CreateUiState() }
-    }
-
     fun onIntent(intent: CreateIntent) {
         when (intent) {
             is CreateIntent.CreateRoom -> {
